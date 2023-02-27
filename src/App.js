1d51178245eb840ec02d64ee748fb2dfd21a9e67
Ignacio <naxete7@hotmail.es>
2023-02-27 16:11:09 +0100

    error resolve

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 
 import './App.css';
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './containers/Home/Home';
+import Books from './containers/Books/Books';
+import BooksDetails from './containers/BooksDetails/BooksDetails.jsx';
+
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Books />} />
+          <Route path="/" element={<BooksDetails/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Home.css'
+
+
+
+
+
+
+
+const Home = () => {
+    
+    return (
+        <div className="homeDesign">
+            Soy el Home
+        </div>
+    )
+
+}
+
+
+export default Home;
